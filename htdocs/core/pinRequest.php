1ad5ec7ae14c22579c9796b403277f568293d184
juan ignacio <juanignaciomln@gmail.com>
2014-10-31 00:53:34 -0300

    Anti infinite loop protection implemented in pinRequest.php

--- htdocs/core/pinRequest.php
+++ htdocs/core/pinRequest.php
@@ -8,19 +8,35 @@
 			include 'utils/pintools.php';
 			include 'utils/gentools.php';
 
+			//Static
+			$MAX_ATTEMPTS = 5;
+
+
 			$pincode;
+			$retry = 0;
 			//We are gonna keep generating PINs until we get one that is unique
 			do { 
 				$pincode = generatePincode();	
-			} while ( checkExistance($pincode) );
+				$retry++;
+				echo "Attempt ".$retry." of ".$MAX_ATTEMPTS." ...<br>";
+			} while ( checkExistance($pincode) and ($retry <= $MAX_ATTEMPTS) );
 
-			$password = generatePassword();
+			//We add some logic to prevent infinit loops
+			if ($retry > $MAX_ATTEMPTS) {
+				echo "Error: Max attempts reach";
+			}
+			else {
 
-			//Insert the new $pincode in the db
-			makePin($pincode, $password);
-			
-			//Finally, a unique PIN is returned
-			echo "PIN: ".$pincode."<br> Password: ".$password;
+				$password = generatePassword();
+
+				//Insert the new $pincode in the db
+				makePin($pincode, $password);
+				
+				//Finally, a unique PIN is returned
+				echo "PIN: ".$pincode."<br> Password: ".$password;
+
+			}
+
 		?>
 
 	</body>
