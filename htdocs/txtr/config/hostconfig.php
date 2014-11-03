5597a402ab50593b2cc75a596ef853a6d6ffc3c4
juan ignacio <juanignaciomln@gmail.com>
2014-11-03 00:56:50 -0300

    PIN JSON object has now a token value. Added $pintokenkey to hostconfig.

--- htdocs/txtr/config/hostconfig.php
+++ htdocs/txtr/config/hostconfig.php
@@ -1,4 +1,5 @@
 <?php
 	// Virtual host config
 	$virtualhost = 'droidko.com';
+	$pintokenkey = 'E}tr*S1Rm7@~y*,|Pt%Dgmg|AL5dn.lIk-<3]Vk6c``]0/!.h0QB(*`D8,|t^|/j';
 ?>
--- htdocs/txtr/utils/jsontools.php
+++ htdocs/txtr/utils/jsontools.php
@@ -2,8 +2,8 @@
 
 	//This is an overloaded function, every argument is optional. If an argmuent is not supplied,
 	//the function will use the default values specified
-	//Arguments: pinJSON($success, $pincode, $password, $host, $timestamp)
-	function pinJSON($success = false, $pincode = null, $password = null, $host = null, $timestamp = null) {
+	//Arguments: pinJSON($success, $pincode, $password, $host, $timestamp, $token)
+	function pinJSON($success = null, $pincode = null, $password = null, $host = null, $timestamp = null, $token = null) {
 		
 		$jsonarray = array(
 				"type"=>"pin",					//Type of object: PIN
@@ -11,7 +11,8 @@
 				"pincode"=>$pincode,			//Alphanumeric PIN that identifies this object
 				"password"=>$password,			//PIN's password
 				"host"=>$host,					//PIN's host, ie: droidko.com
-				"timestamp"=>$timestamp			//Date of PIN's creation
+				"timestamp"=>$timestamp,		//Date of PIN's creation
+				"token"=>$token					//The token is the result of applying a hashing algorithm to the pincode					
 				);
 
 		return json_encode($jsonarray);
