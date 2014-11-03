1b9d109cdecf87622fd4c287376f16d5a1653126
juan ignacio <juanignaciomln@gmail.com>
2014-11-03 01:02:42 -0300

    New generatePinToken in gentools for the authentification of new PINs

--- htdocs/txtr/utils/gentools.php
+++ htdocs/txtr/utils/gentools.php
@@ -1,17 +1,23 @@
 <?php
 
-	function generatePassword () {
 	//Generate random 8 chars (A-Z, a-z, 0-9) password
-	$alphabet = array_merge(range('A', 'Z'), range('a','z'), range(0, 9));
-	$password =	$alphabet[rand(0,61)].
-				$alphabet[rand(0,61)].
-				$alphabet[rand(0,61)].
-				$alphabet[rand(0,61)].
-				$alphabet[rand(0,61)].
-				$alphabet[rand(0,61)].
-				$alphabet[rand(0,61)].
-				$alphabet[rand(0,61)];
-	return $password;
+	function generatePassword () {
+		$alphabet = array_merge(range('A', 'Z'), range('a','z'), range(0, 9));
+		$password =	$alphabet[rand(0,61)].
+					$alphabet[rand(0,61)].
+					$alphabet[rand(0,61)].
+					$alphabet[rand(0,61)].
+					$alphabet[rand(0,61)].
+					$alphabet[rand(0,61)].
+					$alphabet[rand(0,61)].
+					$alphabet[rand(0,61)];
+		return $password;
+	}
+
+	//Generate auth token for PINs creation
+	function generatePinToken ($pincode) {
+		include './hostconfig.php'; //The file has the $pintokenkey value
+		return crypt($pincode, $pintokenkey); //crypt(stringToEncrypt, key)
 	}
 
 ?>
