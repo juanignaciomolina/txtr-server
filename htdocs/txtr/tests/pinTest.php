afff8182ffa50df8748c269c871490b957b3645c
juan ignacio <juanignaciomln@gmail.com>
2014-11-05 03:41:40 -0300

    pinTest error handling improved

--- htdocs/txtr/tests/pinTest.php
+++ htdocs/txtr/tests/pinTest.php
@@ -5,6 +5,9 @@
 		EXAMPLE: api.droidko.com/tests/pinTest.php?test=1
 	*/
 
+	//Static
+	$tests = array(1);
+
 	//GET and POST variables recieved through HTTP REST
 	$vars = $_SERVER['QUERY_STRING'];
 
@@ -14,21 +17,25 @@
 	   die();
 	}
 
-	function messageHTML($ans) {
+	function messageHTML($ans, $validTests = []) {
 		switch ($ans) {
 			case 1:
-				return "PIN TEST: No test number specified, please provide one with GET: ?test=number <br>";			
+				return "PIN TEST: No test number specified, please provide one with GET: ?test=" . implode(" or ",$validTests) . "<br>";
+
+			case 2:
+				return "PIN TEST: Invalid test number specified,  please use one of the following tests " . implode(", ",$validTests) . "<br>";			
 			
 			default:
 				return "PIN TEST: Unknown error";
 		}
 	}
 
+	//TODO: create a testJSON type of object
 	function messageJSON($ans) {
 		include '../utils/jsontools.php';
 		switch ($ans) {
 			case 1:
-				return pinJSON(200); //No test number provided	
+				return pinJSON(200); //No test number provided
 			
 			default:
 				return pinJSON(299); //Unknown error
@@ -40,18 +47,17 @@
 		
 		switch ($_GET["test"]) {
 			case 1:
-				redirect("pinRequest.php".$vars);
+				redirect("pinRequest.php?".$vars);
 				break;
 			
 			default: //HTML
-				echo messageHTML(1);
+				echo messageHTML(2, $tests);
 				break;
 		}
 
 	}
 	else {
-		if($output == 'html') {echo messageHTML(1, $methods);}
-		else {echo messageJSON(1);}
+		echo messageHTML(1, $tests);
 	}
 
 ?>
